e2f354d287f7368e1cb41fc89c83fea80c1ade0c
Darren Behan <darrenbehan@hotmail.com>
2020-08-17 10:42:46 +1000

    assigning id to be sent to through AJAX request

--- public/assets/js/whiteboard.js
+++ public/assets/js/whiteboard.js
@@ -34,6 +34,8 @@ $(document).ready(() => {
 
   function dragMoveListener(event) {
     const target = event.target;
+    const id = target.getAttribute("id");
+
     // keep the dragged position in the data-x/data-y attributes
     const x = (parseFloat(target.getAttribute("data-x")) || 0) + event.dx;
     const y = (parseFloat(target.getAttribute("data-y")) || 0) + event.dy;
