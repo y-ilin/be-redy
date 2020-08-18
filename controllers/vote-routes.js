ca2c7d070b01344445e170a6fea1da023939148c
Yi Lin Lee <yilinlee19@gmail.com>
2020-08-18 20:41:38 +1000

    add functionality to track votes in database

---
+++ controllers/vote-routes.js
@@ -0,0 +1,56 @@
+// Requiring our models and passport as we've configured it
+const db = require("../models");
+
+module.exports = function(app) {
+  // In order to create a voting system, we need to create a many-to-many relationship with Sequelize between our
+  // StickyNote and User models. This way, we can track which users vote for which stickies.
+  // Users can vote for many stickies, and each sticky can be voted for by many users.
+  db.StickyNote.belongsToMany(db.User, { through: "Vote" });
+  db.User.belongsToMany(db.StickyNote, { through: "Vote" });
+
+  // Finding a sticky by its id
+  function findSticky(id) {
+    return db.StickyNote.findOne({
+      where: {
+        id: id
+      }
+    });
+  }
+
+  // Finding a user by their id
+  function findUser(id) {
+    return db.User.findOne({
+      where: {
+        id: id
+      }
+    });
+  }
+
+  // When receiving a POST request to /api/vote, log the user's vote by adding
+  // that particular user to that particular sticky's table in the database.
+  app.post("/api/vote", async (req, res) => {
+    try {
+      // Finding the relevant sticky from the Sticky model
+      const sticky = await findSticky(req.body.stickyId);
+
+      // Finding the relevant user from the User model
+      const user = await findUser(req.body.userId);
+
+      // Adding this user to this sticky in the database (this is a Sequelize method)
+      await sticky.addUser(user);
+
+      // Finding how many users have voted for this particular sticky (this is a Sequelize method)
+      const stickyVoteCount = await sticky.countUsers();
+
+      // Returning a JSON message to the client side with the user's email,
+      // the sticky's id, and the sticky's vote count.
+      res.json({
+        userEmail: user.email,
+        stickyId: sticky.id,
+        stickyVoteCount: stickyVoteCount
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+};
---
+++ public/assets/js/vote.js
@@ -0,0 +1,36 @@
+$(document).ready(() => {
+  // Event listener listening for a click on any vote button, then will run voteSticky
+  $(document).on("click", ".voteButton", voteSticky);
+
+  // GET request to figure out which user is logged in
+  function getUserData() {
+    return $.get("/api/user_data");
+  }
+
+  // Function to send POST request to server to log sticky vote
+  async function voteSticky(event) {
+    try {
+      // Getting the stickyId of the sticky clicked on
+      const sticky = event.currentTarget.closest(".draggable");
+      const stickyId = $(sticky).attr("id");
+
+      // Getting the userId of the logged in user
+      const userData = await getUserData();
+      const userId = userData.id;
+
+      // Send post request to server with the stickyId and userId in order to log this vote
+      $.post("/api/vote", { stickyId, userId }, data => {
+        // Find the star vote counter for this sticky
+        const stickyStar = $(sticky).find(".voteCount");
+        // Render the vote count onto the star
+        stickyStar.html(data.stickyVoteCount);
+
+        console.log(
+          `User ${data.userEmail} has voted for sticky #${data.stickyId}`
+        );
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+});
