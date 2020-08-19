e082634e30c2aa1e3e15abcc96d700e65d59b616
Yi Lin Lee <yilinlee19@gmail.com>
2020-08-19 12:56:49 +1000

    deleting irrelevant code

--- public/assets/js/vote.js
+++ public/assets/js/vote.js
@@ -2,11 +2,6 @@ $(document).ready(() => {
   // Event listener listening for a click on any vote button, then will run voteSticky
   $(document).on("click", ".voteButton", voteSticky);
 
-  // GET request to figure out which user is logged in
-  function getUserData() {
-    return $.get("/api/user_data");
-  }
-
   // Function to send POST request to server to log sticky vote
   async function voteSticky(event) {
     try {
@@ -14,12 +9,8 @@ $(document).ready(() => {
       const sticky = event.currentTarget.closest(".draggable");
       const stickyId = $(sticky).attr("id");
 
-      // Getting the userId of the logged in user
-      const userData = await getUserData();
-      const userId = userData.id;
-
       // Send post request to server with the stickyId and userId in order to log this vote
-      $.post("/api/vote", { stickyId, userId }, data => {
+      $.post("/api/vote", { stickyId }, data => {
         // Find the star vote counter for this sticky
         const stickyStar = $(sticky).find(".voteCount");
         // Render the vote count onto the star
